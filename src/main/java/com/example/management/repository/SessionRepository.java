df85863e573409e0ce314a0d961c663840c50530
Anastasiya <n.bogocharova@gmail.com>
2024-08-21 17:10:37 +0300

    sessions removed

--- src/main/java/com/example/management/repository/SessionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.management.repository;
-
-import com.example.management.entity.Session;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SessionRepository extends JpaRepository<Session, Integer> {
-}
--- src/main/java/com/example/management/service/SessionService.java
+++
@@ -1,9 +0,0 @@
-package com.example.management.service;
-
-import com.example.management.repository.SessionRepository;
-import org.springframework.stereotype.Service;
-
-@Service
-public class SessionService {
-    private SessionRepository sessionRepository;
-}
