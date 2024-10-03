dadfe0176260391d9273c7bfaef629c09f593b1b
Anastasiya <n.bogocharova@gmail.com>
2024-10-03 20:18:44 +0300

    fix

--- src/main/java/com/example/management/service/AuthenticationService.java
+++ src/main/java/com/example/management/service/AuthenticationService.java
@@ -3,23 +3,10 @@ package com.example.management.service;
 import com.example.management.dto.JwtAuthenticationResponse;
 import com.example.management.dto.SignInRequest;
 import com.example.management.dto.SignUpRequest;
-import com.example.management.entity.User;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AuthenticationService {
-    private final UserService userService;
-    private final JwtService jwtService;
-    private final AuthenticationManager authenticationManager;
-
-    public AuthenticationService(UserService userService, JwtService jwtService, AuthenticationManager authenticationManager) {
-        this.userService = userService;
-        this.jwtService = jwtService;
-        this.authenticationManager = authenticationManager;
-    }
+public interface AuthenticationService {
 
     /**
      * Регистрация пользователя
@@ -27,15 +14,7 @@ public class AuthenticationService {
      * @param request данные пользователя
      * @return токен
      */
-    public JwtAuthenticationResponse signUp(SignUpRequest request) {
-
-        User user = new User(request.getName(), request.getSurname(), request.getEmail(), request.getPassword());
-
-        userService.create(user);
-
-        var jwt = jwtService.generateToken(user);
-        return new JwtAuthenticationResponse(jwt);
-    }
+    JwtAuthenticationResponse signUp(SignUpRequest request);
 
     /**
      * Аутентификация пользователя
@@ -43,17 +22,5 @@ public class AuthenticationService {
      * @param request данные пользователя
      * @return токен
      */
-    public JwtAuthenticationResponse signIn(SignInRequest request) {
-        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
-                request.getEmail(),
-                request.getPassword()
-        ));
-
-        var user = userService
-                .userDetailsService()
-                .loadUserByUsername(request.getEmail());
-
-        var jwt = jwtService.generateToken(user);
-        return new JwtAuthenticationResponse(jwt);
-    }
+    JwtAuthenticationResponse signIn(SignInRequest request);
 }
